970a51e6ad3f09aa9549a9ccb3dc5ac594aa5a1e
WaffleWaldo <oswaldoortiz0519@gmail.com>
2023-10-21 09:41:27 -0400

    firebase implement at its base form

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { signOut } from "firebase/auth";
+
+    export let user: any;
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/posts">Posts</a>
+    <p>Hello {user.uid}</p>
+    <button on:click={() => signOut}>Sign Out</button>
+</nav>
